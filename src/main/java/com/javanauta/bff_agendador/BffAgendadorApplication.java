13f0a5dd7c8ceba648b1ad751fcac4c69cf1c6f8
Rocketsz777 <alanmarquescelular@gmail.com>
2025-09-22 11:40:12 -0300

    commit inicial , esqueleto do projeto

---
+++ src/main/java/com/javanauta/bff_agendador/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.bff_agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/bff_agendador/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.bff_agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
